fc3ce18f22d3ec929e1200e10c2fdd19c2d91851
leonknight99-flann <leon.knight@flann.com>
2025-08-20 14:37:58 +0100

    Adding class comment definition

--- flann/vi/switch/flann338.py
+++ flann/vi/switch/flann338.py
@@ -1,6 +1,7 @@
 from flann.vi import FlannProgrammable
 
 class Switch338(FlannProgrammable):
+    '''Class for Flann's 338 Switches - Work in Progress'''
     def __init__(self, address: str, timedelay: float=0):# baudrate: int=0, timeout: float=0, timedelay=0, tcp_port: int=0):
         super().__init__(address, timedelay)  # baudrate, timeout, timedelay, tcp_port)
         self._resource.port = address
